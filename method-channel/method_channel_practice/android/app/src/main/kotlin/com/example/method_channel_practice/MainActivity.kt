9900518f9d767e11ab4e6c10f4d1d5f9842f4126
masal9pse <returnymgstokh@icloud.com>
2022-08-31 02:08:08 +0900

    first commit

---
+++ method-channel/method_channel_practice/android/app/src/main/kotlin/com/example/method_channel_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.method_channel_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ method-channel/method_channel_practice/ios/Runner/AppDelegate.swift
@@ -0,0 +1,41 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    // メソッドチャンネル名
+    let METHOD_CHANNEL_NAME = "mukku.com/battery"
+    // FlutterMethodChannel
+    let batteryChannel = FlutterMethodChannel(name: METHOD_CHANNEL_NAME, binaryMessenger: controller as! FlutterBinaryMessenger)
+      batteryChannel.setMethodCallHandler({
+      (call: FlutterMethodCall,result: @escaping FlutterResult)->Void in
+          switch call.method {
+          case "getBatteryLevel":
+              guard let args = call.arguments as? [String: String] else {return}
+              let name = args["name"]!
+              result("\(name) says \(self.receiveBatteryLevel())")
+          default:
+              result(FlutterMethodNotImplemented)
+          }
+      })
+      
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+    private func receiveBatteryLevel() -> Int {
+          let device = UIDevice.current
+          device.isBatteryMonitoringEnabled = true
+          
+          if (device.batteryState == UIDevice.BatteryState.unknown) {
+              return -1
+          } else {
+              return Int(device.batteryLevel * 100)
+          }
+      }
+}
