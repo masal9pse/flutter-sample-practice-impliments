12c0406ee19207d090039e50225cd6f07a28fb22
masal9pse <returnymgstokh@icloud.com>
2022-09-01 00:09:17 +0900

    first commit

--- background/using_flutter_uploader/lib/main.dart
+++ background/using_flutter_uploader/lib/main.dart
@@ -1,115 +1,349 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_uploader/flutter_uploader.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:meta/meta.dart';
+import 'package:path/path.dart';
 
-void main() {
-  runApp(const MyApp());
+const String title = "FileUpload Sample app";
+const String uploadURL =
+    "https://us-central1-flutteruploader.cloudfunctions.net/upload";
+
+const String uploadBinaryURL =
+    "https://us-central1-flutteruploader.cloudfunctions.net/upload/binary";
+
+void main() => runApp(App());
+
+class App extends StatefulWidget {
+  final Widget? child;
+
+  App({Key? key,this.child}) : super(key: key);
+
+  _AppState createState() => _AppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: title,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: UploadScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class UploadItem {
+  final String? id;
+  final String? tag;
+  final MediaType? type;
+  final int? progress;
+  final UploadTaskStatus? status;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  UploadItem({
+    this.id,
+    this.tag,
+    this.type,
+    this.progress = 0,
+    this.status = UploadTaskStatus.undefined,
+  });
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  UploadItem copyWith({required UploadTaskStatus status, required int progress}) => UploadItem(
+      id: this.id,
+      tag: this.tag,
+      type: this.type,
+      status: status ?? this.status,
+      progress: progress ?? this.progress);
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  bool isCompleted() =>
+      this.status == UploadTaskStatus.canceled ||
+      this.status == UploadTaskStatus.complete ||
+      this.status == UploadTaskStatus.failed;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+enum MediaType { Image, Video }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+class UploadScreen extends StatefulWidget {
+  UploadScreen({Key? key}) : super(key: key);
+
+  @override
+  _UploadScreenState createState() => _UploadScreenState();
+}
+
+class _UploadScreenState extends State<UploadScreen> {
+  FlutterUploader uploader = FlutterUploader();
+  StreamSubscription? _progressSubscription;
+  StreamSubscription? _resultSubscription;
+  Map<String, UploadItem> _tasks = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _progressSubscription = uploader.progress.listen((progress) {
+      final task = _tasks[progress.tag];
+      print("progress: ${progress.progress} , tag: ${progress.tag}");
+      if (task == null) return;
+      if (task.isCompleted()) return;
+      setState(() {
+        _tasks[progress.tag] =
+            task.copyWith(progress: progress.progress, status: progress.status);
+      });
+    });
+    _resultSubscription = uploader.result.listen((result) {
+      print(
+          "id: ${result.taskId}, status: ${result.status}, response: ${result.response}, statusCode: ${result.statusCode}, tag: ${result.tag}, headers: ${result.headers}");
+
+      final task = _tasks[result.tag];
+      if (task == null) return;
+
+      setState(() {
+        _tasks[result.tag] = task.copyWith(status: result.status,);
+      });
+    }, onError: (ex, stacktrace) {
+      print("exception: $ex");
+      print("stacktrace: $stacktrace" ?? "no stacktrace");
+      final exp = ex as UploadException;
+      final task = _tasks[exp.tag];
+      if (task == null) return;
+
+      setState(() {
+        _tasks[exp.tag] = task.copyWith(status: exp.status);
+      });
     });
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _progressSubscription?.cancel();
+    _resultSubscription?.cancel();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Plugin example app'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            Container(height: 20.0),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'multipart/form-data uploads',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () => getImage(binary: false),
+                  child: Text("upload image"),
+                ),
+                Container(width: 20.0),
+                RaisedButton(
+                  onPressed: () => getVideo(binary: false),
+                  child: Text("upload video"),
+                )
+              ],
+            ),
+            Container(height: 20.0),
+            Text(
+              'binary uploads',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            Text('this will upload selected files as binary'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () => getImage(binary: true),
+                  child: Text("upload image"),
+                ),
+                Container(width: 20.0),
+                RaisedButton(
+                  onPressed: () => getVideo(binary: true),
+                  child: Text("upload video"),
+                )
+              ],
+            ),
+            Expanded(
+              child: ListView.separated(
+                padding: EdgeInsets.all(20.0),
+                itemCount: _tasks.length,
+                itemBuilder: (context, index) {
+                  final item = _tasks.values.elementAt(index);
+                  print("${item.tag} - ${item.status}");
+                  return UploadItemView(
+                    item: item,
+                    onCancel: cancelUpload,
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider(
+                    color: Colors.black,
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  String _uploadUrl({required bool binary}) {
+    if (binary) {
+      return uploadBinaryURL;
+    } else {
+      return uploadURL;
+    }
+  }
+
+  Future getImage({@required bool binary}) async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      final String filename = basename(image.path);
+      final String savedDir = dirname(image.path);
+      final tag = "image upload ${_tasks.length + 1}";
+      var url = _uploadUrl(binary: binary);
+      var fileItem = FileItem(
+        filename: filename,
+        savedDir: savedDir,
+        fieldname: "file",
+      );
+
+      var taskId = binary
+          ? await uploader.enqueueBinary(
+              url: url,
+              file: fileItem,
+              method: UploadMethod.POST,
+              tag: tag,
+              showNotification: true,
+            )
+          : await uploader.enqueue(
+              url: url,
+              data: {"name": "john"},
+              files: [fileItem],
+              method: UploadMethod.POST,
+              tag: tag,
+              showNotification: true,
+            );
+
+      setState(() {
+        _tasks.putIfAbsent(
+            tag,
+            () => UploadItem(
+                  id: taskId,
+                  tag: tag,
+                  type: MediaType.Video,
+                  status: UploadTaskStatus.enqueued,
+                ));
+      });
+    }
+  }
+
+  Future getVideo({required bool binary}) async {
+    var video = await ImagePicker.pickVideo(source: ImageSource.gallery);
+    if (video != null) {
+      final String savedDir = dirname(video.path);
+      final String filename = basename(video.path);
+      final tag = "video upload ${_tasks.length + 1}";
+      final url = _uploadUrl(binary: binary);
+
+      var fileItem = FileItem(
+        filename: filename,
+        savedDir: savedDir,
+        fieldname: "file",
+      );
+
+      var taskId = binary
+          ? await uploader.enqueueBinary(
+              url: url,
+              file: fileItem,
+              method: UploadMethod.POST,
+              tag: tag,
+              showNotification: true,
+            )
+          : await uploader.enqueue(
+              url: url,
+              data: {"name": "john"},
+              files: [fileItem],
+              method: UploadMethod.POST,
+              tag: tag,
+              showNotification: true,
+            );
+
+      setState(() {
+        _tasks.putIfAbsent(
+            tag,
+            () => UploadItem(
+                  id: taskId,
+                  tag: tag,
+                  type: MediaType.Video,
+                  status: UploadTaskStatus.enqueued,
+                ));
+      });
+    }
+  }
+
+  Future cancelUpload(String id) async {
+    await uploader.cancel(taskId: id);
+  }
+}
+
+typedef CancelUploadCallback = Future<void> Function(String id);
+
+class UploadItemView extends StatelessWidget {
+  final UploadItem? item;
+  final CancelUploadCallback? onCancel;
+
+  UploadItemView({
+    Key? key,
+    this.item,
+    this.onCancel,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final progress = item!.progress!.toDouble() / 100;
+    final widget = item!.status == UploadTaskStatus.running
+        ? LinearProgressIndicator(value: progress)
+        : Container();
+    final buttonWidget = item!.status == UploadTaskStatus.running
+        ? Container(
+            height: 50,
+            width: 50,
+            child: IconButton(
+              icon: Icon(Icons.cancel),
+              onPressed: () => onCancel!(item!.id!),
+            ),
+          )
+        : Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(item!.tag!),
+              Container(
+                height: 5.0,
+              ),
+              Text(item!.status!.description),
+              Container(
+                height: 5.0,
+              ),
+              widget
+            ],
+          ),
+        ),
+        buttonWidget
+      ],
     );
   }
 }
